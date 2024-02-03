6447112f7d348ab91d56db3963d9aab563e0785a
Thomas2424 <113831322+Tomas-618@users.noreply.github.com>
2024-02-04 00:38:40 +0400

    Begin

---
+++ Scripts/Controller/Basic/BasicController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public abstract class BasicController : MonoBehaviour, IMovable, IRotatable
+{
+    private void Update()
+    {
+        Control();
+    }
+
+    public abstract void Move();
+
+    public abstract void Rotate();
+
+    protected virtual void Control()
+    {
+        Move();
+        Rotate();
+    }
+}
